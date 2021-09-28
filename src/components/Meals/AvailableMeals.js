30939809390613dcdbcc461d7ad2ef1503bf195b
Tajeshwar <tkhara@gmail.com>
2021-09-28 13:50:36 +0530

    Error handling done on AvailableMeals for getting data from Firebase.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
+
 import Card from '../UI/Card'
 import MealItem from './MealItem/MealItem'
 import classes from './AvailableMeals.module.css'
@@ -6,22 +7,27 @@ import classes from './AvailableMeals.module.css'
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [httpError, setHttpError] = useState()
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        'https://react-meals-369c6-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json'
+        'https://react-meals-369c6-default-rtdb.asia-southeast1.firebasedatabase.app/meals'
       )
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
+
       const responseData = await response.json()
 
-      // Transform received object to an array
       const loadedMeals = []
 
       for (const key in responseData) {
         loadedMeals.push({
           id: key,
-          description: responseData[key].description,
           name: responseData[key].name,
+          description: responseData[key].description,
           price: responseData[key].price,
         })
       }
@@ -30,13 +36,24 @@ const AvailableMeals = () => {
       setIsLoading(false)
     }
 
-    fetchMeals()
+    fetchMeals().catch((error) => {
+      setIsLoading(false)
+      setHttpError(error.message)
+    })
   }, [])
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     )
   }
