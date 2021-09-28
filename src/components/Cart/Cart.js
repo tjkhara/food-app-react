39dbd61818897fb6bac75927ff3765f8ce8d14a6
Tajeshwar <tkhara@gmail.com>
2021-09-28 22:36:37 +0530

    Food App now complete.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,23 +1,60 @@
-import { useContext } from 'react';
+import React, { useContext, useState } from 'react'
 
-import Modal from '../UI/Modal';
-import CartItem from './CartItem';
-import classes from './Cart.module.css';
-import CartContext from '../../store/cart-context';
+import Modal from '../UI/Modal'
+import CartItem from './CartItem'
+import classes from './Cart.module.css'
+import CartContext from '../../store/cart-context'
+import Checkout from './Checkout'
 
 const Cart = (props) => {
-  const cartCtx = useContext(CartContext);
+  const [isCheckout, setIsCheckout] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [error, setError] = useState(null)
+  const [didSubmit, setDidSubmit] = useState(false)
 
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-  const hasItems = cartCtx.items.length > 0;
+  const cartCtx = useContext(CartContext)
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
+  const hasItems = cartCtx.items.length > 0
 
   const cartItemRemoveHandler = (id) => {
-    cartCtx.removeItem(id);
-  };
+    cartCtx.removeItem(id)
+  }
 
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem(item);
-  };
+    cartCtx.addItem(item)
+  }
+
+  const orderHandler = () => {
+    setIsCheckout(true)
+  }
+
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true)
+    setError(null)
+    try {
+      const response = await fetch(
+        'https://react-meals-369c6-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            user: userData,
+            orderedItem: cartCtx.items,
+          }),
+        }
+      )
+
+      setIsSubmitting(false)
+      setDidSubmit(true)
+      cartCtx.clearCart()
+
+      if (!response.ok) {
+        throw new Error('Something went wrong')
+      }
+    } catch (error) {
+      setError(error.message)
+    }
+  }
 
   const cartItems = (
     <ul className={classes['cart-items']}>
@@ -32,23 +69,56 @@ const Cart = (props) => {
         />
       ))}
     </ul>
-  );
+  )
 
-  return (
-    <Modal onClose={props.onClose}>
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes['button--alt']} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  )
+
+  const cartModalContent = (
+    <React.Fragment>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
+      {isCheckout && (
+        <Checkout onCancel={props.onClose} onConfirm={submitOrderHandler} />
+      )}
+      {!isCheckout && modalActions}
+      {error && <p>{error}</p>}
+    </React.Fragment>
+  )
+
+  const isSubmittingModalContent = <p>Sending order data...</p>
+
+  const didSubmitModalContent = (
+    <React.Fragment>
+      <p>Successfully sent the order!</p>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
+        <button className={classes.button} onClick={props.onClose}>
           Close
         </button>
-        {hasItems && <button className={classes.button}>Order</button>}
       </div>
-    </Modal>
-  );
-};
+    </React.Fragment>
+  )
 
-export default Cart;
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
+    </Modal>
+  )
+}
+
+export default Cart
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -12,7 +12,7 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        'https://react-meals-369c6-default-rtdb.asia-southeast1.firebasedatabase.app/meals'
+        'https://react-meals-369c6-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json'
       )
 
       if (!response.ok) {
